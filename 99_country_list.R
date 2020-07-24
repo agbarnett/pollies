92a4621ce256d3b24825f63a1e038a9ab3e4f9ab
Adrian Barnett <a.barnett@qut.edu.au>
2020-07-24 21:21:19 +1000

    Delete 99_country_list.R

--- 99_country_list.R
+++
@@ -1,9 +0,0 @@
-# 99_country_list.R
-# simple list of countries called by other programs
-# December 2019
-
-## dropped Japan in December 2019
-# all countries:
-countries = c('Austria','Australia','Canada','Italy','France','Germany','Netherlands','NZ','Switzerland','UK','USA')
-# countries with imputed life table data
-countries.imputed = c('Austria','Canada','Germany','NZ','USA','UK')
